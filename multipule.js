0c70d4e0f546e1bf6e978e3ec4449366b2b3ce05
asm-ayesha <ayesha219333@gmail.com>
2025-11-20 12:52:33 +0600

    multiple condition logical operators &&,||

---
+++ multipule.js
@@ -0,0 +1,28 @@
+const salary = 25000;
+const height = 60;
+const isBcs = true;
+const iscar = false;
+
+// if (salary > 22000){
+//     console.log("su pathro");
+// }
+//  else{
+//     console.log("onno pathro khuji");
+//  }
+
+
+// if (salary > 22000 && height > 70){
+//     console.log("su pathro");
+// }
+//  else{
+//     console.log("onno pathro khuji");
+//  }
+
+
+
+if ((salary > 22000 && height > 70 ) || (isBcs == true && iscar == false)){
+    console.log("su pathro");
+}
+ else{
+    console.log("onno pathro khuji");
+ }
