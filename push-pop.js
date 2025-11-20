63cb828137aab0746150552b2f46c14d0ea3d18f
asm-ayesha <ayesha219333@gmail.com>
2025-11-20 20:05:41 +0600

    add & remove element from arrays using push, pop,shift,ubshift

---
+++ push-pop.js
@@ -0,0 +1,15 @@
+let numbers = [12, 60, 54, 70]
+console.log(numbers)
+
+// push add
+
+numbers.push(10);
+console.log(numbers)
+numbers.push(10,50,65,75,43);
+console.log(numbers)
+
+// pop remove the last element
+numbers.pop();
+let out = numbers.pop();
+console.log(numbers)
+console.log(out)
---
+++ shift-unshift.js
@@ -0,0 +1,10 @@
+let names = ['abul', 'kabul', 'habul', 'dabul', 'bulbul']
+console.log(names)
+
+// shift remove 1st index
+names.shift();
+console.log(names)
+
+//unshift add 1st index
+names.unshift('tabul');
+console.log(names)
