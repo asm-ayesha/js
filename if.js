087ee4e477985b72738917e09823b78ca252ccf7
asm-ayesha <ayesha219333@gmail.com>
2025-11-20 12:30:28 +0600

    consitional branching if else condition

---
+++ if.js
@@ -0,0 +1,13 @@
+var biriyaniPrice = 500 ;
+if(biriyaniPrice > 300){
+    console.log("i will buy");
+}
+
+var weight = 40 ;
+if (weight < 20 ){
+    console.log("i will carry it by myself");
+}
+
+else{
+    console.log("i will rent a richsaw");
+}
