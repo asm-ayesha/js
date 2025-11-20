da15a9c50a936850c415d16e349d6893ab3de69b
asm-ayesha <ayesha219333@gmail.com>
2025-11-20 13:26:50 +0600

    nasted if else condition

---
+++ nasted.js
@@ -0,0 +1,19 @@
+const money = -20 ;
+if (money > 300){
+    console.log("bro you are rich!!!!");
+}
+else{
+    if (money > 100){
+        console.log("tmi boro lok o na . gorib o na");
+    }
+    else{
+        if(money > 0){
+            console.log("kola kha calcium bara");
+        }
+
+        else{
+            
+                console.log("tui amr bondhu na")
+            }
+        }
+    }
